f921931810bce37dfea3f7a6b5676d6d89cd3e71
Alex <alexhoyosf080302@gmail.com>
2021-07-08 16:12:11 -0700

    Bug de foto de perfil en usuarios arreglado

--- resources/js/models/User.js
+++ resources/js/models/User.js
@@ -7,7 +7,7 @@ class User {
     getProfilePic(){
 
         if(this.profile_pic === undefined){
-            this.profile_pic.url = 'default.png'
+            return API_URL + 'media/usr/default.png'
         }
 
         return API_URL + 'media/usr/' +  this.id + '/' + this.profile_pic.url
